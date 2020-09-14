393db9b2576fbea14e3498b3876edcbbce79cebb
rupam999 <rupamchakraborty999@gmail.com>
2020-09-14 16:03:16 +0530

    CROSS Solved

--- src/api/Config.ts
+++ src/api/Config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const BASE_URL = 
-    "https://e-commerce-pwa-backend.herokuapp.com/";
+    "https://cors-anywhere.herokuapp.com/https://e-commerce-pwa-backend.herokuapp.com/";
     // "http://localhost:8000";
 export const LOGIN = "/api/user/login";
 
