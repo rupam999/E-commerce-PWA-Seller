82b36d767c7d1a9745e28c557ab5e6078eea5fba
rupam999 <rupamchakraborty999@gmail.com>
2021-05-20 10:20:17 +0530

    Desktop Structure Added with Dashboard

--- src/Screens/Desktop/Content/DesktopSellerHome.tsx
+++ src/Screens/Desktop/Content/DesktopSellerHome.tsx
@@ -13,78 +13,78 @@ const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
 export const DesktopSellerHome = () => {
-    const [collapsed, setCollapsed] = useState(false);
-    const history = useHistory();
+  const [collapsed, setCollapsed] = useState(false);
+  const history = useHistory();
 
-    const onCollapse = collapsed => {
-      console.log(collapsed);
-      setCollapsed(collapsed);
-    };
+  const onCollapse = collapsed => {
+    console.log(collapsed);
+    setCollapsed(collapsed);
+  };
 
-    const sendDate = () => {
-      return Date();
-    }
+  const sendDate = () => {
+    return Date();
+  }
 
-    return (
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
-          {/* Logo Area */}
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1" icon={<HomeOutlined />} onClick={() => history.push("/seller")}>
-              Home
-            </Menu.Item>
-            <SubMenu key="sub1" icon={<PlusCircleOutlined />} title="Add New Item">
-                <Menu.Item 
-                  key="3"
-                  onClick={() => history.push("/seller/electronics")}
-                  >
-                  Electronics
-                </Menu.Item>
-                <Menu.Item 
-                  key="4"
-                  >
-                  Fashion
-                </Menu.Item>
-                <Menu.Item 
-                  key="5"
-                  >
-                  Grocery
-                </Menu.Item>
-            </SubMenu>
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+        {/* Logo Area */}
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1" icon={<HomeOutlined />} onClick={() => history.push("/seller")}>
+            Home
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<PlusCircleOutlined />} title="Add New Item">
             <Menu.Item 
-              key="9" 
-              icon={<UserOutlined />}
-              onClick={() => history.push("/")}
+              key="3"
+              onClick={() => history.push("/seller/electronics")}
               >
-              Logout
+              Electronics
             </Menu.Item>
-          </Menu>
-        </Sider>
-
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }} />
-          <Content style={{ margin: '0 16px' }}>
-            <div 
-              className="site-layout-background" 
-              style={{marginTop: 10, minHeight: 500, background: '#fff'}}
+            <Menu.Item 
+              key="4"
               >
-              <Switch>
-                <Route 
-                  exact 
-                  path="/seller" 
-                  component={() => <DekstopDashboard />} 
+              Fashion
+            </Menu.Item>
+            <Menu.Item 
+              key="5"
+              >
+              Grocery
+            </Menu.Item>
+          </SubMenu>
+          <Menu.Item 
+            key="9" 
+            icon={<UserOutlined />}
+            onClick={() => history.push("/")}
+            >
+            Logout
+          </Menu.Item>
+        </Menu>
+      </Sider>
+
+       <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: '0 16px' }}>
+          <div 
+            className="site-layout-background" 
+            style={{marginTop: 10, minHeight: 500, background: '#fff'}}
+            >
+            <Switch>
+              <Route 
+                exact 
+                path="/seller" 
+                component={() => <DekstopDashboard />} 
                 />
-                
-                <Route
-                  exact
-                  path="/seller/electronics"
-                  component={() => <DesktopAddElectronics />}
-                />
-                </Switch>
-              </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>{sendDate().toString()}</Footer>
-        </Layout>
+              
+              <Route
+                exact
+                path="/seller/electronics"
+                component={() => <DesktopAddElectronics />}
+              />
+              </Switch>
+            </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>{sendDate().toString()}</Footer>
       </Layout>
-    );
+    </Layout>
+  );
 }
